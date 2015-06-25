3b1beb1b044d076091fbd82666fc58932b116aff
n70George <1507238@qq.com>
2015-06-25 11:46:37 +0800

    done

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,5 +1,15 @@
 package com.coolweather.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.coolweather.app.db.CoolWeatherDB;
@@ -8,72 +18,132 @@ import com.coolweather.app.model.County;
 import com.coolweather.app.model.Province;
 
 public class Utility {
-	
-	public synchronized static boolean handleProvinceReponse(CoolWeatherDB coolWeatherDB,
-			String response){
-		
-		if(!TextUtils.isEmpty(response)){
-			String [] allProvinces=response.split(",");
-			if(allProvinces!=null && allProvinces.length>0){
-				for(String p:allProvinces){
-					String [] array=p.split("\\|");
-					Province province=new Province();
+
+	public synchronized static boolean handleProvinceReponse(
+			CoolWeatherDB coolWeatherDB, String response) {
+
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
+					String[] array = p.split("\\|");
+					Province province = new Province();
 					province.setCode(array[0]);
 					province.setName(array[1]);
-					
+
 					coolWeatherDB.saveProvince(province);
 				}
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-	
-	
-	public synchronized static boolean handleCityReponse(CoolWeatherDB coolWeatherDB,
-			String response,int provinceId){
-		
-		if(!TextUtils.isEmpty(response)){
-			String [] allCities=response.split(",");
-			if(allCities!=null && allCities.length>0){
-				for(String p:allCities){
-					String [] array=p.split("\\|");
-					City city=new City();
+
+	public synchronized static boolean handleCityReponse(
+			CoolWeatherDB coolWeatherDB, String response, int provinceId) {
+
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null && allCities.length > 0) {
+				for (String p : allCities) {
+					String[] array = p.split("\\|");
+					City city = new City();
 					city.setCode(array[0]);
 					city.setName(array[1]);
 					city.setProvinceId(provinceId);
-					
+
 					coolWeatherDB.saveCity(city);
 				}
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-	
-	
-	public synchronized static boolean handleCountyReponse(CoolWeatherDB coolWeatherDB,
-			String response,int cityId){
-		
-		if(!TextUtils.isEmpty(response)){
-			String [] allCounties=response.split(",");
-			if(allCounties!=null && allCounties.length>0){
-				for(String p:allCounties){
-					String [] array=p.split("\\|");
-					County county=new County();
+
+	public synchronized static boolean handleCountyReponse(
+			CoolWeatherDB coolWeatherDB, String response, int cityId) {
+
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCounties = response.split(",");
+			if (allCounties != null && allCounties.length > 0) {
+				for (String p : allCounties) {
+					String[] array = p.split("\\|");
+					County county = new County();
 					county.setCode(array[0]);
 					county.setName(array[1]);
 					county.setCityId(cityId);
-					
+
 					coolWeatherDB.saveCounty(county);
 				}
 				return true;
 			}
 		}
 		return false;
-		
+
+	}
+
+	/**
+	 * 解析服务器返回的Json数据，并将解析出的数据存储到本地
+	 * 
+	 * @param context
+	 * @param response
+	 */
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * save weatherinfo to SharedPreferences
+	 * 
+	 * @param context
+	 *            context
+	 * @param cityName
+	 *            cityName
+	 * @param weatherCode
+	 *            weatherCode
+	 * @param temp1
+	 *            temp1
+	 * @param temp2
+	 *            temp2
+	 * @param weatherDesp
+	 *            weatherDesp
+	 * @param publishTime
+	 *            publishTime
+	 */
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+
 	}
 
 }
